5701d360bf6302858a0676ff7ed0f444091d54f0
Vivek Sharma <161102201+geekovic@users.noreply.github.com>
2025-05-03 18:12:54 +0530

    Create ArithmeticOperations.java

---
+++ LAB1/ArithmeticOperations.java
@@ -0,0 +1,17 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a = 20;              // Declare two numbers
+        int b = 6;
+        int sum = a + b;          // Perform arithmetic operations
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        int remainder = a % b;
+        System.out.println("a = " + a + ", b = " + b);               // Display results
+        System.out.println("Sum (a + b) = " + sum);
+        System.out.println("Difference (a - b) = " + difference);
+        System.out.println("Product (a * b) = " + product);
+        System.out.println("Quotient (a / b) = " + quotient);
+        System.out.println("Remainder (a % b) = " + remainder);
+ } 
+}
